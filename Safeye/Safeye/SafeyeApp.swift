dd8502eac40f1cb190cf4965ba7aac097e15a2f1
fuka-innovations <fuad.kalhori@gmail.com>
2022-04-01 12:15:18 +0300

    initialized iOS project

---
+++ Safeye/Safeye/SafeyeApp.swift
@@ -0,0 +1,20 @@
+//
+//  SafeyeApp.swift
+//  Safeye
+//
+//  Created by Koulu on 1.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct SafeyeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
