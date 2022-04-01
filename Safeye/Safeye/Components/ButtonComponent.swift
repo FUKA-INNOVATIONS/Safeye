4477f8bb30803501865320eefba20c282789b4cc
fuka-innovations <fuad.kalhori@gmail.com>
2022-04-01 15:00:03 +0300

    added empty components, read file comments!

---
+++ Safeye/Safeye/Components/ButtonComponent.swift
@@ -0,0 +1,11 @@
+//
+//  ButtonComponent.swift
+//  Safeye
+//
+//  Created by FUKA on 1.4.2022.
+//
+
+/* An other option would be to have folder for each component including View and modifiers */
+
+
+import Foundation
