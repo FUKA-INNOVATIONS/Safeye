f904a40587cb5c10b7194be1e2af48f9763f18fa
fuka-innovations <fuad.kalhori@gmail.com>
2022-04-06 18:56:24 +0300

    donno

--- Safeye/Safeye/Views/LoginView.swift
+++ Safeye/Safeye/Views/LoginView.swift
@@ -18,30 +18,28 @@ struct LoginView: View {
     
     var body: some View {
         
-        ScrollView {
-            VStack {
-                InputFieldComponent(title: "Email address", inputText: $email)
-                SecureInputFieldComponent(title: "Password", secureText: $password)
-                
-                BasicButtonComponent(label: "Sign In", action: {
-                    // Email and password not provided
-                    guard !email.isEmpty, !password.isEmpty else {
-                        return
-                    }
-                    // Email and password is provided, try to sign the user in
-                    viewModel.signIn(email: email, password: password)
-                    print("LOGIN attempt")
-                    
-                })
-                
-                NavigationLink("Create a new account", destination: RegisterView(viewModel: viewModel))
-                    .padding()
-            }
-            .navigationTitle("Sign in")
-            .alert("Login failed", isPresented: $viewModel.signinError) {
-                Button("OK", role: .cancel) { }
-            }
+        VStack {
+            InputFieldComponent(title: "Email address", inputText: $email)
+            SecureInputFieldComponent(title: "Password", secureText: $password)
+            
+            BasicButtonComponent(label: "Sign In", action: {
+                // Email and password not provided
+                guard !email.isEmpty, !password.isEmpty else {
+                    return
+                }
+                // Email and password is provided, try to sign the user in
+                viewModel.signIn(email: email, password: password)
+            })
+            
+            // Go to Register view
+            NavigationLink("Create a new account", destination: RegisterView(viewModel: viewModel))
+                .padding()
         }
+        // Show alert on login failure
+        .alert("Login failed", isPresented: $viewModel.signinError) {
+            Button("OK", role: .cancel) { }
+        }
+        .navigationTitle("Sign in") // TODO: this creates warnings
     }
 }
 
