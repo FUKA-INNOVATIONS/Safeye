b2a6db2c52e8775ba6fb92930d78ba6f83a9eed5
gintaresaali <70065528+gintaresaali@users.noreply.github.com>
2022-04-09 17:19:27 +0300

    UI for adding trusted contact ready, search field component ready

--- Safeye/Safeye/Views/AddContactView.swift
+++ Safeye/Safeye/Views/AddContactView.swift
@@ -2,22 +2,108 @@
 //  AddContactView.swift
 //  Safeye
 //
-//  Created by FUKA on 1.4.2022.
+//  Created by gintare on 9.4.2022.
 //
-
-
-// Maybe just a component (modal/popup) and not a separate view ???
 
 import SwiftUI
 
 struct AddContactView: View {
+    @Binding var isShowing: Bool
+    @State var searchInput: String = ""
+    var isFound: Bool = true
+    
+    @State private var curHeight: CGFloat = 500
+    let minHeight: CGFloat = 500
+    let maxHeight: CGFloat = 600
+    
+    let startOpacity: Double = 0.4
+    let endOpacity: Double = 0.9
+    
+    var dragPercentage: Double {
+        let res = Double((curHeight - minHeight) / (maxHeight - minHeight))
+        return max(0, min(1, res))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            if isShowing {
+                Color.black
+                    .opacity(startOpacity + (endOpacity - startOpacity) * dragPercentage)
+                    .ignoresSafeArea()
+                    .onTapGesture { isShowing = false}
+                
+                mainView
+                    .transition(.move(edge: .bottom))
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
+    }
+    
+    var mainView: some View {
+        VStack {
+            ZStack {
+                Capsule()
+                    .frame(width: 40, height: 6)
+            }
+            .frame(height: 40)
+            .frame(maxWidth: .infinity)
+            .background(Color.white.opacity(0.00001))
+            .gesture(dragGesture)
+            
+            ZStack {
+                VStack {
+                    SearchFieldComponent(searchInput: $searchInput)
+                    // Button(action: { isShowing = false}, label: {Text("Close")})
+                    Spacer()
+                    if isFound {
+                        AvatarComponent(size: 100)
+                        Text("FirstName LastName")
+                    }
+                    BasicButtonComponent(label: "Add", action: { print("Added")})
+                    Spacer()
+                }
+            }
+            .frame(maxHeight: .infinity)
+            .padding(.bottom, 35)
+        }
+        .frame(height: curHeight)
+        .frame(maxWidth: .infinity)
+        .background(
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                Rectangle()
+                    .frame(height: curHeight / 2)
+            }
+                .foregroundColor(.white)
+        )
+        .onDisappear { curHeight = minHeight}
+    }
+    
+    @State private var prevDragTransition = CGSize.zero
+    
+    var dragGesture: some Gesture {
+        DragGesture(minimumDistance: 0, coordinateSpace: .global)
+            .onChanged { val in
+                let dragAmount = val.translation.height - prevDragTransition.height
+                if curHeight > maxHeight || curHeight < minHeight {
+                    curHeight -= dragAmount / 6
+                } else {
+                    curHeight -= dragAmount
+                }
+                
+                prevDragTransition = val.translation
+            }
+            .onEnded { val in
+                prevDragTransition = .zero
+            }
     }
 }
 
+
+
 struct AddContactView_Previews: PreviewProvider {
     static var previews: some View {
-        AddContactView()
+        AddContactView(isShowing: .constant(true))
     }
 }
--- Safeye/Safeye/Views/ProfileView.swift
+++ Safeye/Safeye/Views/ProfileView.swift
@@ -9,23 +9,33 @@ import SwiftUI
 
 struct ProfileView: View {
     @EnvironmentObject var AuthenticationViewModel: AuthenticationViewModel
+    @State private var showModal = false
+
     var body: some View {
-   
+        ZStack {
         VStack {
             BasicButtonComponent(label: "Sign out") {
                 // Sign out button
                 AuthenticationViewModel.signOut()
-                        }
+            }
             Group{
-            Spacer()
+                Spacer()
                 Text("FirstName LastName").font(.system(size: 25, weight: .bold))
-            AvatarComponent(size: 80)
-            Spacer()
+                AvatarComponent(size: 80)
+                Spacer()
             }
             Group {
                 Text("My trusted contacts").font(.system(size: 18, weight: .semibold))
-                ListViewComponent(item: "avatar", size: 50)
-            Spacer()
+                HStack{
+                    ListViewComponent(item: "avatar", size: 50)
+                    Button(action: {
+                        showModal = true
+                        print("modal: \($showModal.wrappedValue)")
+                    })
+                    { Image("icon-add") }
+                    Spacer(minLength: 20)
+                }
+                Spacer()
             }
             UserDetailsComponent()
             Spacer()
@@ -38,6 +48,9 @@ struct ProfileView: View {
                 Spacer()
             }
             //a button will be placed here?
+        }
+            AddContactView(isShowing: $showModal)
+
         }
     }
 }
