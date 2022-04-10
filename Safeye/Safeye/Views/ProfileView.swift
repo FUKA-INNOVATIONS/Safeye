4c601f1be1483d5536c752051859e4d98f06244a
fuka-innovations <fuad.kalhori@gmail.com>
2022-04-10 13:26:59 +0300

    fixed

--- Safeye/Safeye/Views/ProfileView.swift
+++ Safeye/Safeye/Views/ProfileView.swift
@@ -15,45 +15,54 @@ struct ProfileView: View {
     
     var body: some View {
         
-        VStack {
-            Group{
-                Spacer()
-                Text("\(ProfileVM.profileDetails?.fullName ?? "No name")")
-                    .font(.system(size: 25, weight: .bold))
-                
-                // Show edit profile view in a Modal
-                BasicButtonComponent(label: "Edit profile details") {
-                    showingEditProfile = true
+        ZStack {
+            VStack {
+                Group{
+                    Spacer()
+                    Text("\(ProfileVM.profileDetails?.fullName ?? "No name")")
+                        .font(.system(size: 25, weight: .bold))
+                    
+                    // Show edit profile view in a Modal
+                    BasicButtonComponent(label: "Edit profile details") {
+                        showingEditProfile = true
+                    }
+                    .sheet(isPresented: $showingEditProfile) {
+                        ProfileEditView()
+                    }
+                    
+                    AvatarComponent(size: 80)
+                    Spacer()
                 }
-                .sheet(isPresented: $showingEditProfile) {
-                    ProfileEditView()
+                Group {
+                    Text("My trusted contacts").font(.system(size: 18, weight: .semibold))
+                    HStack{
+                        ListViewComponent(item: "avatar", size: 50)
+                        Button(action: {
+                            showingAddContact = true
+                            print("modal: ($showingAddContact.wrappedValue)")
+                        })
+                        { Image("icon-add") }
+                        Spacer(minLength: 20)
+                    }
+                    Spacer()
+                }
+                UserDetailsComponent()
+                Spacer()
+                Group {
+                    Text("My safe spaces").font(.system(size: 18, weight: .semibold))
+                    
+                    //size with icons doesn't work properly, will figure this out later
+                    ListViewComponent(item: "safeSpace", size: 40)
+                    Spacer()
                 }
                 
-                AvatarComponent(size: 80)
-                Spacer()
             }
-            Group {
-                Text("My trusted contacts").font(.system(size: 18, weight: .semibold))
-                ListViewComponent(item: "avatar", size: 50)
-                Spacer()
+            .onAppear {
+                ProfileVM.getProfile()
             }
-            UserDetailsComponent()
-            Spacer()
-            Group {
-                Text("My safe spaces").font(.system(size: 18, weight: .semibold))
-                
-                //size with icons doesn't work properly, will figure this out later
-                ListViewComponent(item: "safeSpace", size: 40)
-                Spacer()
-            }
-            
             AddContactView(isShowing: $showingAddContact)
-            
         }
-        .onAppear {
-            ProfileVM.getProfile()
-        }
-    
+        
     }
 }
 
