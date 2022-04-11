adaf093379b30063076b0156e12944d8334b4077
dfallow <dfallow@protonmail.com>
2022-04-11 14:47:23 +0300

    added test link to ProfileView

--- Safeye/Safeye/Views/EventView.swift
+++ Safeye/Safeye/Views/EventView.swift
@@ -1,20 +1,73 @@
 //
-//  EventView.swift
+//  TrackingModeView.swift
 //  Safeye
 //
-//  Created by FUKA on 1.4.2022.
+//  Created by dfallow on 10.4.2022.
 //
 
 import SwiftUI
 
-struct EventView: View {
+struct TrackingModeView: View {
+    
+    @StateObject private var viewModel = EventViewModel()
+    @State var panicMode: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+        VStack {
+            
+            Text("Current Status: \(viewModel.mode)")
+                .font(.largeTitle)
+                .padding(.top, 100)
+            
+            Spacer()
+            viewModel.mode == "Tracking" ?
+            // User is currently in tracking mode, presses panic button for help
+            Button(action: {
+                // Actions after panic button Has been pressed
+                
+                viewModel.activatePanicMode()
+                panicMode = true
+                
+                }) {
+                    TrackingModeButtonComponent(panicmode: $panicMode)
+            }
+            :
+            // User is in panic mode presses are you safe button
+            Button(action: {
+                
+                viewModel.disablePanicMode()
+                panicMode = false
+                
+                }) {
+                    TrackingModeButtonComponent(panicmode: $panicMode)
+                }
+            //PanicButtonComponent()
+            Spacer()
+        
+            //Send value of tracking: true to map view
+            NavigationLink("View Map", destination: MapView())
+                .padding()
+                
+            // Needs to be replace with button
+            NavigationLink("End", destination: ProfileView())
+            Button(action: {
+            }) {
+                NavigationLink(destination: ContentView()) {
+                    Text("Disable Tracking")
+                }
+            }
+        
+            Spacer()
+            
+        }.ignoresSafeArea()
+        
     }
 }
 
-struct EventView_Previews: PreviewProvider {
+struct TrackingModeView_Previews: PreviewProvider {
     static var previews: some View {
-        EventView()
+        TrackingModeView()
+.previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
