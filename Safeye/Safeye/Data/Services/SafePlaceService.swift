15f1684173ac01b815ba84e5b4991d3e75a1a1f6
fuka-innovations <fuad.kalhori@gmail.com>
2022-04-23 23:33:59 +0300

    fixed real-time-update bugs

--- Safeye/Safeye/Data/Services/SafePlaceService.swift
+++ Safeye/Safeye/Data/Services/SafePlaceService.swift
@@ -18,7 +18,7 @@ class SafePlaceService {
     // Add new safe place to database
     func addSafePlace(_ place: SafePlaceModel) -> Bool {
         do {
-            _ = try safePlaceDB.addDocument(from: place)
+            _ = try self.safePlaceDB.addDocument(from: place)
             return true
         }
         catch {
@@ -30,38 +30,36 @@ class SafePlaceService {
     
     // Fetch list of safe places from the database
     func fetchUserSafePlaces(of userID: String) {
-        self.appState.safePlaces.removeAll() // empty safe places collection
+        self.appState.safePlaces.removeAll() // empty safe places collection, move out, not belogning here, fo in all services
         self.appState.locations.removeAll() // empty locations list
-        
-        self.safePlaceDB.whereField("ownerId", isEqualTo: userID).getDocuments() { places, error in
-            if let error = error { print("Error in fetchUserSafePlaces: \(error)") ; return }
-            else {
-                if places!.isEmpty { print("Current user has no safe places"); return }
-                if let places = places {
-                    for place in places.documents {
-                        print("Safe places of current user: \(place)")
-                        DispatchQueue.main.async {
-                            do {
-                                let convertedPlace = try place.data(as: SafePlaceModel.self)
-                                self.appState.safePlaces.append(convertedPlace)
-                                let isOwner = (convertedPlace.ownerId == AuthenticationService.getInstance.currentUser!.uid) // temporary solution, not belogning here
-                                let location = Location(name: convertedPlace.name, coordinate: CLLocationCoordinate2D(latitude: convertedPlace.latitude, longitude: convertedPlace.longitude), own: isOwner)
-                                self.appState.locations.append(location)
-                            } catch {
-                                print("Error fetchUserSafePlaces 2: \(error)")
-                            }
+        DispatchQueue.main.async {
+            self.safePlaceDB.whereField("ownerId", isEqualTo: userID).getDocuments() { places, error in
+                if let error = error { print("Error in fetchUserSafePlaces: \(error)") ; return }
+                else {
+                    if places!.isEmpty { print("Current user has no safe places"); return }
+                    if let places = places {
+                        for place in places.documents {
+                            print("Safe places of current user: \(place)")
+                                do {
+                                    let convertedPlace = try place.data(as: SafePlaceModel.self)
+                                    self.appState.safePlaces.append(convertedPlace)
+                                    let isOwner = (convertedPlace.ownerId == AuthenticationService.getInstance.currentUser!.uid) // temporary solution, not belogning here
+                                    let location = Location(name: convertedPlace.name, coordinate: CLLocationCoordinate2D(latitude: convertedPlace.latitude, longitude: convertedPlace.longitude), own: isOwner)
+                                    self.appState.locations.append(location)
+                                } catch {
+                                    print("Error fetchUserSafePlaces 2: \(error)")
+                                }
                         }
                     }
                 }
             }
         }
-        
     }
     
     
     // // delete specific safe place from the database
     func deleteSafePlaceByID(_ safePlaceID: String) {
-        safePlaceDB.document(safePlaceID).delete() { error in
+        self.safePlaceDB.document(safePlaceID).delete() { error in
             if let error = error {
                 print("Error deleting safe place: \(error)")
             } else {
