5e7bd5583c087fd7d7ad4d145d1cd7651325e954
gintaresaali <70065528+gintaresaali@users.noreply.github.com>
2022-04-25 22:34:40 +0300

    added info to settings view

--- Safeye/Safeye/Views/SettingsView.swift
+++ Safeye/Safeye/Views/SettingsView.swift
@@ -18,75 +18,88 @@ struct SettingsView: View {
     @State var tcList = []
     @State var contacts: [ProfileModel] = [ProfileModel]()
     @State var fetchClicked = 0
-    
+    @State var isInfoDetailShowing = false
+    @State var section: String?
 
     @AppStorage("isDarkMode") private var isDarkMode = false
 
         
     var body: some View {
         
-        return VStack {
-            
-            VStack{
-                Picker("Mode", selection: $isDarkMode){
-                Text("Light")
-                    .tag(false)
-                Text("Dark")
-                    .tag(true)
+        Form {
+            Section("Settings") {
+                VStack{
+                    Picker("Mode", selection: $isDarkMode){
+                    Text("Light")
+                        .tag(false)
+                    Text("Dark")
+                        .tag(true)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
-                Spacer()
-            }
-            
-//            BasicButtonComponent(label: "Sign out") { // Sign out button
-            BasicButtonComponent(label: translationManager.signOut) {
-                AuthVM.signOut()
-            }
-            
-            NavigationLink("Playground") {
-                PlayGroundView()
-            }
-            
-            NavigationLink("Go To Map") {
-                MapView()
-            }
-            
-            /* NavigationLink("\(appState.eventCurrentUser == nil ? "Create event" : "You have an event")") {
-                if appState.eventCurrentUser == nil {
-                    CreateEventView()
-                } else {
-                    EventView()
-                }
-            } */
-            
-            // Button to update users home coordinates
-            // TODO add popup info box when pressed
-            if CLLocationManager().authorizationStatus.rawValue == 4 {
-                Button("Update Home Coordinates") {
-                    ProfileVM.updateUserHomeCoordinates()
-                }
-            }
-            HStack{
-                Text("Connection code: \(appState.profile?.connectionCode ?? "No code")")
-                Button(action: {
-                    let pasteboard = UIPasteboard.general
-                    pasteboard.string = appState.profile?.connectionCode
-                    print("Copied")
-//                }, label: {Text("Copy")})
-                }, label: {Text(translationManager.copyBtn)})
                 
-            }.padding()
+    //            BasicButtonComponent(label: "Sign out") { // Sign out button
+                Button(translationManager.signOut) {
+                    AuthVM.signOut()
+                }
+                
+    //            NavigationLink("Playground") {
+    //                PlayGroundView()
+    //            }
+    //
+    //            NavigationLink("Go To Map") {
+    //                MapView()
+    //            }
+                
+                /* NavigationLink("\(appState.eventCurrentUser == nil ? "Create event" : "You have an event")") {
+                    if appState.eventCurrentUser == nil {
+                        CreateEventView()
+                    } else {
+                        EventView()
+                    }
+                } */
+                
+                // Button to update users home coordinates
+                // TODO add popup info box when pressed
+                if CLLocationManager().authorizationStatus.rawValue == 4 {
+                    Button("Update Home Coordinates") {
+                        ProfileVM.updateUserHomeCoordinates()
+                    }
+                }
+            }
             
-            SettingsListViewComponent(settingsView: true)
+            
+            Section("Information") {
+                List{
+                    Text("About Safeye")
+                        .onTapGesture {
+                            section = "about"
+                            isInfoDetailShowing = true
+                        }
+                    Text("Concepts explained")
+                        .onTapGesture {
+                            section = "concepts"
+                            isInfoDetailShowing = true
+                        }
+                    Text("Privacy")
+                        .onTapGesture {
+                            section = "privacy"
+                            isInfoDetailShowing = true
+                        }
+                }
+                .sheet(isPresented: $isInfoDetailShowing) {
+                    InfoDetailView(section: $section)
+                }
+            }
+            
         }
         .onAppear {
             //EventVM.getEventsOfTrustedContacts() // to check for panic events
             //EventVM.getEventsOfCurrentUser()
             EventVM.sendNotification()
         }
-        
-        
+        .navigationBarHidden(true)
     }
 }
 
