3b32eaaaa446138c2a1dc0b65b95f17498c2f132
Pavlo Leinonen <leinonenko@gmail.com>
2022-04-26 20:56:36 +0300

    added loading view

--- Safeye/Safeye/ContentView.swift
+++ Safeye/Safeye/ContentView.swift
@@ -28,6 +28,7 @@ struct ContentView: View {
             
             VStack {
                 //TODO: BUG : after registration, create profile is not displayed, it is diaplyed on next app start
+                if self.appState.appLoading { AnimationLottieView(lottieJson: "eye") }
                 if appState.profile == nil {
                     // User has no profile, create new one
                     Text(translationManager.textProfile)
@@ -38,6 +39,7 @@ struct ContentView: View {
                         ProfileEditView()
                     }
                 } else {
+                    
                     // User has profile -> show the app
                     VStack {
                         // Show alert on every view when someone in panic mode
