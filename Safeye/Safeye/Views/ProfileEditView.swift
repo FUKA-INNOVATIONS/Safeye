e63f65858ab66803e8491519f001a49d134446de
Pavlo Leinonen <leinonenko@gmail.com>
2022-04-28 14:50:17 +0300

    code clean up

--- Safeye/Safeye/Views/ProfileEditView.swift
+++ Safeye/Safeye/Views/ProfileEditView.swift
@@ -52,157 +52,157 @@ struct ProfileEditView: View {
         
         return VStack() {
             if appState.profile != nil {
-//                Text("Please fill the fileds you want to update")
+                //Text("Please fill the fileds you want to update")
                 Text(translationManager.textProfileUpdate)
                 
             } else {
-//                Text("Please fill all fields to create new profile")
+                //Text("Please fill all fields to create new profile")
                 Text(translationManager.textProfileBlanc)
             }
             
             
             ScrollView {
-            HStack { // Display profile photo
-                if selectedPhoto != nil { // if user has picked a new photo, show that one
-                    ProfileImageComponent(size: 60, avatarImage: selectedPhoto!)
-                } else if appState.userPhoto != nil { // if user hasn't selected a new photo, fetch one from DB if they have it
-                    ProfileImageComponent(size: 60, avatarImage: appState.userPhoto!)
-                } else { // otherwise show placeholder image
-                    ProfileImageComponent(size: 40, avatarImage: UIImage(imageLiteralResourceName: "avatar-placeholder"))
+                HStack { // Display profile photo
+                    if selectedPhoto != nil { // if user has picked a new photo, show that one
+                        ProfileImageComponent(size: 60, avatarImage: selectedPhoto!)
+                    } else if appState.userPhoto != nil { // if user hasn't selected a new photo, fetch one from DB if they have it
+                        ProfileImageComponent(size: 60, avatarImage: appState.userPhoto!)
+                    } else { // otherwise show placeholder image
+                        ProfileImageComponent(size: 40, avatarImage: UIImage(imageLiteralResourceName: "avatar-placeholder"))
+                    }
+                    Button {
+                        isImagePickerShowing = true
+                    } label: {
+                        Text("Select profile photo")
+                    }
+                    
                 }
-                Button {
-                    isImagePickerShowing = true
-                } label: {
-                    Text("Select profile photo")
+                .sheet(isPresented: $isImagePickerShowing, onDismiss: nil) {
+                    // Image picker
+                    ImagePicker(selectedPhoto: $selectedPhoto, isImagePickerShowing: $isImagePickerShowing)  // this opens ImagePicker helper
                 }
                 
-            }
-            .sheet(isPresented: $isImagePickerShowing, onDismiss: nil) {
-                // Image picker
-                ImagePicker(selectedPhoto: $selectedPhoto, isImagePickerShowing: $isImagePickerShowing)  // this opens ImagePicker helper
-            }
-            
-            
-                HStack {
+                //
+                HStack { // Picker for country of residence
                     Text("Country")
                     Spacer()
-                        Section {
-                            Picker(selection: $selectedIndex, label: Text("")) {
-                                ForEach(0 ..< countries.count) {
-                                    Text(self.countries[$0])
+                    Section {
+                        Picker(selection: $selectedIndex, label: Text("")) {
+                            ForEach(0 ..< countries.count) {
+                                Text(self.countries[$0])
+                            }
+                        }
+                    }
+                }.padding()
+                
+                
+                HStack{
+                    //InputFieldComponent(title: "Full name", inputText: $fullName)
+                    InputFieldComponent(title: translationManager.fullNameTitle, inputText: $fullName)
+                }
+                
+                HStack{
+                    //InputFieldComponent(title: "Address", inputText: $address)
+                    InputFieldComponent(title: translationManager.addressTitle, inputText: $address)
+                }
+                HStack{
+                    //InputFieldComponent(title: "Birthday", inputText: $birthday)
+                    InputFieldComponent(title: translationManager.birthdayTitle, inputText: $birthday)
+                }
+                HStack{
+                    VStack(alignment: .leading){
+                        
+                        // Select blood type
+                        Section { // Sections can have header and footer
+                            //Picker("Blood type", selection: $bloodType) {
+                            Picker(translationManager.bloodTypeTitle, selection: $bloodType) {
+                                ForEach(bloodTypes, id: \.self) {
+                                    Text($0)
                                 }
                             }
+                            //.pickerStyle(.segmented)
+                            .pickerStyle(SegmentedPickerStyle())
+                            .padding()
+                        } header: {
+                            //Text("Blood type")
+                            Text(translationManager.bloodTitle)
                         }
-                }.padding()
-            
-            
-            HStack{
-//                InputFieldComponent(title: "Full name", inputText: $fullName)
-                InputFieldComponent(title: translationManager.fullNameTitle, inputText: $fullName)
-            }
-            
-            HStack{
-//                InputFieldComponent(title: "Address", inputText: $address)
-                InputFieldComponent(title: translationManager.addressTitle, inputText: $address)
-            }
-            HStack{
-//                InputFieldComponent(title: "Birthday", inputText: $birthday)
-                InputFieldComponent(title: translationManager.birthdayTitle, inputText: $birthday)
-            }
-            HStack{
-                VStack(alignment: .leading){
-                    
-                    // Select blood type
-                    Section { // Sections can have header and footer
-//                        Picker("Blood type", selection: $bloodType) {
-                        Picker(translationManager.bloodTypeTitle, selection: $bloodType) {
-                            ForEach(bloodTypes, id: \.self) {
-                                Text($0)
-                            }
-                        }
-                        //.pickerStyle(.segmented)
-                        .pickerStyle(SegmentedPickerStyle())
                         .padding()
-                    } header: {
-//                        Text("Blood type")
-                        Text(translationManager.bloodTitle)
+                        
+                        //InputFieldComponent(title: "Illness", inputText: $illness)
+                        //InputFieldComponent(title: "Allergies", inputText: $allergies)
+                        InputFieldComponent(title: translationManager.illnessTitle, inputText: $illness)
+                        InputFieldComponent(title: translationManager.allergiesTitle, inputText: $allergies)
+                        
                     }
-                    .padding()
-                    
-//                    InputFieldComponent(title: "Illness", inputText: $illness)
-//                    InputFieldComponent(title: "Allergies", inputText: $allergies)
-                    InputFieldComponent(title: translationManager.illnessTitle, inputText: $illness)
-                    InputFieldComponent(title: translationManager.allergiesTitle, inputText: $allergies)
-                    
                 }
-            }
-            
-            
-//            BasicButtonComponent(label: "Save & go back") { // Button to save profile details
-            BasicButtonComponent(label: translationManager.saveBtn) {
-                print("Save profile details pressed")
                 
-                if appState.profile == nil { // User has no profile, create new one
-                    print("User has no PROFILE, add new profile")
+                
+                //BasicButtonComponent(label: "Save & go back") { // Button to save profile details
+                BasicButtonComponent(label: translationManager.saveBtn) {
+                    print("Save profile details pressed")
                     
-                    // User has not filled all form fields
-                    // TODO: show different alert when no photo is selected
-                    if fullName.count < 1 || address.count < 1 || birthday.count < 1 || bloodType.count < 1 || illness.count < 1 || allergies.count < 1 || selectedPhoto == nil {
-                        showEmptyFieldAlert = true // Show alert box
-                        return
+                    if appState.profile == nil { // User has no profile, create new one
+                        print("User has no PROFILE, add new profile")
+                        
+                        // User has not filled all form fields
+                        // TODO: show different alert when no photo is selected
+                        if fullName.count < 1 || address.count < 1 || birthday.count < 1 || bloodType.count < 1 || illness.count < 1 || allergies.count < 1 || selectedPhoto == nil {
+                            showEmptyFieldAlert = true // Show alert box
+                            return
+                        }
+                        
+                        // set avatar path/name to a random string that will be stored in profile // TODO: change avatar path name > userID
+                        avatar = "avatars/\(UUID().uuidString).jpg"
+                        // upload the image
+                        FileVM.uploadPhoto(selectedPhoto: selectedPhoto, avatarUrlFetched: avatar)
+                        
+                        // Insert new profile data into the database
+                        ProfileVM.createProfile(fullName, address, birthday, bloodType, illness, allergies, avatar)
+                        
+                        // presentationMode.wrappedValue.dismiss() // Close modal and return to ContentView()
+                        dismiss()
+                        
+                        
+                    } else { // User has profile, update existing
+                        print("User has a profile, update it")
+                        
+                        // User has not filled all form fields
+                        if fullName.count < 1 || address.count < 1 || birthday.count < 1 || bloodType.count < 1 || illness.count < 1 || allergies.count < 1 {
+                            showEmptyFieldAlert = true // Show alert box
+                            return
+                        }
+                        
+                        // set avatar path/name to current avatar path fetched from user's profile
+                        let avatarUrlFetched = appState.profile!.avatar
+                        // upload the image
+                        FileVM.uploadPhoto(selectedPhoto: selectedPhoto, avatarUrlFetched: avatarUrlFetched)
+                        self.avatar = avatarUrlFetched
+                        
+                        // Update profile data in the database
+                        ProfileVM.updateProfile(fullName, address, birthday, bloodType, illness, allergies, avatar)
+                        
+                        // presentationMode.wrappedValue.dismiss() // Close modal and return to ProfileView
+                        appState.userPhoto = selectedPhoto
+                        dismiss()
                     }
-                    
-                    // set avatar path/name to a random string that will be stored in profile // TODO: change avatar path name > userID
-                    avatar = "avatars/\(UUID().uuidString).jpg"
-                    // upload the image
-                    FileVM.uploadPhoto(selectedPhoto: selectedPhoto, avatarUrlFetched: avatar)
-                    
-                    // Insert new profile data into the database
-                    ProfileVM.createProfile(fullName, address, birthday, bloodType, illness, allergies, avatar)
-                    
-                    // presentationMode.wrappedValue.dismiss() // Close modal and return to ContentView()
-                    dismiss()
-                    
-                    
-                } else { // User has profile, update existing
-                    print("User has a profile, update it")
-                    
-                    // User has not filled all form fields
-                    if fullName.count < 1 || address.count < 1 || birthday.count < 1 || bloodType.count < 1 || illness.count < 1 || allergies.count < 1 {
-                        showEmptyFieldAlert = true // Show alert box
-                        return
-                    }
-                    
-                    // set avatar path/name to current avatar path fetched from user's profile
-                    let avatarUrlFetched = appState.profile!.avatar
-                    // upload the image
-                    FileVM.uploadPhoto(selectedPhoto: selectedPhoto, avatarUrlFetched: avatarUrlFetched)
-                    self.avatar = avatarUrlFetched
-                    
-                    // Update profile data in the database
-                    ProfileVM.updateProfile(fullName, address, birthday, bloodType, illness, allergies, avatar)
-                    
-                    // presentationMode.wrappedValue.dismiss() // Close modal and return to ProfileView
-                    appState.userPhoto = selectedPhoto
-                    dismiss()
                 }
+                .alert(isPresented: $showEmptyFieldAlert) { // Alert user about emptu fields
+                    Alert(
+                        //title: Text("Fill all fields"),
+                        //message: Text("Please fill all fields")
+                        title: Text(translationManager.fieldAlertTitle),
+                        message: Text(translationManager.fieldAlertText)
+                    )
+                }
+                
+                
+                
             }
-            .alert(isPresented: $showEmptyFieldAlert) { // Alert user about emptu fields
-                Alert(
-//                    title: Text("Fill all fields"),
-//                    message: Text("Please fill all fields")
-                    title: Text(translationManager.fieldAlertTitle),
-                    message: Text(translationManager.fieldAlertText)
-                )
-            }
-            
             
             
         }
-        
-        
     }
-}
 }
 
 
