566fbfbdb93790a82649e01ca14992500866b7be
dfallow <dfallow@protonmail.com>
2022-05-02 17:33:53 +0300

    MapViewModel commented

--- Safeye/Safeye/ViewModels/MapViewModel.swift
+++ Safeye/Safeye/ViewModels/MapViewModel.swift
@@ -5,10 +5,18 @@
 //  Created by FUKA on 1.4.2022.
 //
 
+/*
+    Class that handles the functionality of the mapView containing the users SafeSpaces.
+    Retrieves and displays the users safeSpaces (a combination of their own and their trusted
+    contacts  home addresses) and displays them on the map. Also hanldes the device location
+    permissions and creates/defines the properties of the location manager that handles the
+    users own location.
+ */
 import MapKit
 import Foundation
 import SwiftUI
 
+// Location struct which stores the information about the users safeSpaces
 struct Location: Identifiable {
     let id = UUID()
     let name: String
@@ -17,7 +25,6 @@ struct Location: Identifiable {
 }
 
 class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    //static let instance = MapViewModel() ; private init() {}
     
     // If the users does not allow location, map should center on Helsinki
     @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 60.170, longitude: 24.941), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
@@ -26,6 +33,7 @@ class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     private var appState = Store.shared
     var locationManager: CLLocationManager?
     
+    // Creates and defines location manager if location permissions are enabled
     func checkIfLocationServicesIsEnabled() {
         if CLLocationManager.locationServicesEnabled() {
             locationManager = CLLocationManager()
@@ -38,6 +46,7 @@ class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
         }
     }
     
+    // asks user for permission for device location, then centers map on their location
     private func checkLocationAuthorization() {
         guard let locationManager = locationManager else { return }
         
@@ -56,6 +65,7 @@ class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
         }
     }
     
+    // Re-checks permissions if they have changed
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         checkLocationAuthorization()
     }
